2db63576a0ae6b8d228f87508713875f61bfd53c
Roghayeh Rezaei <roghayeh20rz@gmail.com>
2026-08-19 21:51:02 -0700

    add gaussian blur

--- src/main.py
+++ src/main.py
@@ -7,3 +7,10 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,100,200)
 
 cv2.imwrite("../images/output.jpg",edges)
+
+
+
+##اینجا فیچر بلر رو اد میکنیم
+blur = cv2.GaussianBlur(gray,(5,5),0)
+
+edges = cv2.Canny(blur,100,200)
